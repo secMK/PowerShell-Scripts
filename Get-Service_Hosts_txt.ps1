1608be8679fbe330c0091e271cb24b7c39b459f1
secMK <86817592+secMK@users.noreply.github.com>
2021-07-15 15:36:29 +1000

    Update Get-Service_Hosts_txt.ps1

--- Get-Service_Hosts_txt.ps1
+++ Get-Service_Hosts_txt.ps1
@@ -1,3 +1,3 @@
-$server = Get-Content -path C:\Temp\computers.txt
+$RHOSTS = Get-Content -path C:\Temp\RHOSTS.txt
 
-$server | foreach { (Get-Service -Name spooler -computername $_) | Select-Object MachineName, Status, Name, DisplayName } | Export-Csv -Path C:\Temp\Get-Service_Servers.csv
+$RHOSTS | foreach { (Get-Service -Name spooler -computername $_) | Select-Object MachineName, Status, Name, DisplayName } | Export-Csv -Path C:\Temp\Get-Service_Servers.csv
